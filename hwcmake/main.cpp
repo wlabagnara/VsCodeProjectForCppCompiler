bab4bd8e610a638e6fc71bcc94d89f4378547f48
wlabagnara <wlabagnara@gmail.com>
2023-10-18 08:33:28 -0400

    adding new hello world cmake example project.

---
+++ hwcmake/main.cpp
@@ -0,0 +1,12 @@
+
+// References: 
+//    https://code.visualstudio.com/docs/cpp/config-mingw
+//    https://code.visualstudio.com/docs/cpp/cmake-linux
+// 
+
+#include <iostream>
+
+int main(int, char**){
+    std::cout << "Hello, from hwcmake!\n";
+    std::cout << "Hello again, from hwcmake!\n";
+}
